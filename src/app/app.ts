65edcf7821d81a89e1c5929ee03b3f318f792c7f
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-10-06 08:16:48 -0300

    :sparkles: feat: desenvolvimento da seção de habilidades

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,137 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+
+interface ItemHabilidade {
+  icone: string;
+  titulo: string;
+  descricao?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
 })
-export class App implements OnInit {
-  private readonly title = inject(Title);
+export class App {
+  public itensHabilidades: ItemHabilidade[] = [
+    {
+      icone: 'bi-window-fullscreen',
+      titulo: 'Web Design',
+      descricao: `Design e desenvolvimento de sistemas web atraentes e responsivos.`,
+    },
+    {
+      icone: 'bi-server',
+      titulo: 'Aplicações Back-End',
+      descricao: `Desenvolvimento de sistemas robustos para solucionar problemas simples ou complexos.`,
+    },
+    {
+      icone: 'bi-graph-up',
+      titulo: 'Testes Automatizados',
+      descricao: `Criação de testes automatizados que facilitam a manutenção e melhoram as entregas das aplicações.`,
+    },
+    {
+      icone: 'bi-wrench',
+      titulo: 'Engenharia de Software',
+      descricao: `Planejamento e execução de sistemas multi-camadas, integrando várias tecnologias de forma organizada e escalável.`,
+    },
+    {
+      icone: 'bi-people',
+      titulo: 'Liderança',
+      descricao: `Habilidade de liderar e tomar decisões técnicas em projetos em equipe.`,
+    },
+    {
+      icone: 'bi-building-up',
+      titulo: 'Implantação',
+      descricao: `Implantação e manutenção de sistemas em servidores VPS e em nuvem.`,
+    },
+  ];
 
-  ngOnInit(): void {
-    this.title.setTitle('Angular Vite Starter');
-  }
+  public itensHabilidades2: ItemHabilidade[] = [
+    {
+      icone: 'https://skillicons.dev/icons?i=html&theme=dark',
+      titulo: 'HTML',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=css&theme=dark',
+      titulo: 'CSS',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=scss&theme=dark',
+      titulo: 'SCSS',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=js&theme=dark',
+      titulo: 'JavaScript',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=ts&theme=dark',
+      titulo: 'TypeScript',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=bootstrap&theme=dark',
+      titulo: 'Bootstrap',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=angular&theme=dark',
+      titulo: 'Angular',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=rxjs&theme=dark',
+      titulo: 'RxJS',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=net&theme=dark',
+      titulo: '.NET',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=cs&theme=dark',
+      titulo: 'C#',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=selenium&theme=dark',
+      titulo: 'Selenium',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=cypress&theme=dark',
+      titulo: 'Cypress',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=sqlite&theme=dark',
+      titulo: 'SQLite',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=postgres&theme=dark',
+      titulo: 'Postgres',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=redis&theme=dark',
+      titulo: 'Redis',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=docker&theme=dark',
+      titulo: 'Docker',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=powershell&theme=dark',
+      titulo: 'PowerShell',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=git&theme=dark',
+      titulo: 'Git',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=github&theme=dark',
+      titulo: 'GitHub',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=azure&theme=dark',
+      titulo: 'Azure',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=visualstudio&theme=dark',
+      titulo: 'Visual Studio',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=vscode&theme=dark',
+      titulo: 'VSCode',
+    },
+  ];
 }
