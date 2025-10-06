96135ffea6a4c0874d6cdcf968eab42c7ff9725e
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-10-06 08:43:43 -0300

    :sparkles: feat: desenvolvimento da seção portfólio

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,15 @@ interface ItemHabilidade {
   descricao?: string;
 }
 
+interface ItemPortfolio {
+  titulo: string;
+  descricao: string;
+  urlImagem: string;
+  urlGif: string;
+  urlRepositorio: string;
+  stack: string[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
@@ -134,4 +143,37 @@ export class App {
       titulo: 'VSCode',
     },
   ];
+
+  public itensPortfolio: ItemPortfolio[] = [
+    {
+      titulo: 'Controle de Medicamentos',
+      urlImagem: 'https://i.imgur.com/OYyV8iY.gif',
+      urlGif: 'https://i.imgur.com/OYyV8iY.gif',
+      urlRepositorio:
+        'https://github.com/estevaosantosribeiro/controle-de-medicamentos',
+      stack: [
+        'https://skillicons.dev/icons?i=html&theme=light',
+        'https://skillicons.dev/icons?i=css&theme=light',
+        'https://skillicons.dev/icons?i=cs&theme=light',
+        'https://skillicons.dev/icons?i=net&theme=light',
+      ],
+      descricao: `O sistema de Controle de Medicamentos apresenta o conceito de gerenciamento de um posto de saúde, com o controle de fornecimento, estoque, funcionários, pacientes e retirada de prescrições — com foco na rastreabilidade das movimentações de estoque.`,
+    },
+    {
+      titulo: 'Gestão de Equipamentos',
+      urlImagem: 'https://i.imgur.com/AcU0BxT.gif',
+      urlGif: 'https://i.imgur.com/AcU0BxT.gif',
+      urlRepositorio:
+        'https://github.com/estevaosantosribeiro/gestao-de-equipamentos',
+      stack: [
+        'https://skillicons.dev/icons?i=html&theme=light',
+        'https://skillicons.dev/icons?i=css&theme=light',
+        'https://skillicons.dev/icons?i=cs&theme=light',
+        'https://skillicons.dev/icons?i=net&theme=light',
+      ],
+      descricao: `O sistema de Gestão de Equipamentos controla o estoque de um almoxarifado, com a gestão de equipamentos e o registro de chamados de manutenção que eventualmente ocorrem durante o uso dos equipamentos. Também são inclusas informações sobre os fabricantes dos equipamentos para contato de suporte ou compras.`,
+    },
+  ];
+
+  public itemPortfolioSelecionado?: ItemPortfolio;
 }
